580e121f2c75394d12fd2fe5214eb591b856fe79
EPWebsite <65266294+EPWebsite@users.noreply.github.com>
2021-03-09 03:57:55 +0000

    update

---
+++ pages/blog.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="contact-page" class="page-wrapper contact-page content-page">
+    <main-section theme="one-column">
+      <template v-slot:default>
+        <posts-grid />
+      </template>
+    </main-section>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: `Contact | ${this.$siteConfig.siteName}`
+    }
+  },
+  components: {}
+}
+</script>
